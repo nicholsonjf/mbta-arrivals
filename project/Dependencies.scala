9f29c68a3c8f7ef8be48544fe4d034aefcae1b83
James Nicholson <jnicholson@hsph.harvard.edu>
2022-12-04 02:54:04 -0500

    PureConfig working

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,10 +5,14 @@ object Dependencies {
 
   val AkkaVersion = "2.7.0"
   val AkkaHttpVersion = "10.4.0"
+  val pureconfigVersion = "0.15.0"
   lazy val core = Seq(
     "com.lightbend.akka" %% "akka-stream-alpakka-sse" % "5.0.0",
     "com.typesafe.akka" %% "akka-stream" % AkkaVersion,
     "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
-    "com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion
+    "com.typesafe.akka" %% "akka-actor" % AkkaVersion,
+    "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2",
+    "ch.qos.logback" % "logback-classic" % "1.2.3",
+    "com.github.pureconfig" %% "pureconfig" % pureconfigVersion
   )
 }
--- src/main/scala/arrivals/Arrivals.scala
+++ src/main/scala/arrivals/Arrivals.scala
@@ -1,11 +1,12 @@
 package arrivals
 
-import akka.actor.typed.ActorSystem
+import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.sse.ServerSentEvent
 import akka.http.scaladsl.model.{HttpEntity, HttpRequest, HttpResponse, Uri}
 import akka.stream.alpakka.sse.scaladsl.EventSource
-import arrivals.AkkaStreamUtils
+import arrivals.{AkkaStreamUtils, ConfigUtils}
+import pureconfig.generic.auto._
 import akka.stream.scaladsl._
 import akka.Done
 import akka.NotUsed
@@ -16,8 +17,16 @@ import akka.http.scaladsl.model.headers.RawHeader
 import scala.util.{Try, Success, Failure}
 import scala.concurrent.{Await, Future}
 import scala.concurrent.duration.Duration
+import com.typesafe.scalalogging.{LazyLogging}
+
+case class ArrivalsAppConfig(
+  predictionsURI: String,
+  APIKey: String
+)
 
 object MBTA_Arrivals {
+
+  implicit val conf = ConfigUtils.loadAppConfig[ArrivalsAppConfig]("arrivals")
 
   import AkkaStreamUtils.defaultActorSystem._
 
@@ -25,17 +34,23 @@ object MBTA_Arrivals {
     val req_with_headers = req.withHeaders(
       List(
         RawHeader("Accept", "text/event-stream"),
-        RawHeader("X-API-Key", "API_KEY")
+        RawHeader("X-API-Key", conf.APIKey)
       )
     )
-    Http().singleRequest(req_with_headers)
+    val responseFuture: Future[HttpResponse] = Http().singleRequest(req_with_headers)
+    responseFuture
+      .onComplete {
+        case Success(res) => println(res)
+        case Failure(_)   => sys.error("something wrong")
+      }
+    responseFuture
   }
 
   def main(args: Array[String]): Unit = {
 
     val eventSource: Source[ServerSentEvent, NotUsed] =
       EventSource(
-        uri = Uri("https://api-v3.mbta.com/predictions/?filter\\[route\\]=Red&include=schedule,stop"),
+        uri = Uri(conf.predictionsURI),
         sendHttp,
         initialLastEventId = Some("2"),
         retryDelay = 1.second
@@ -44,7 +59,7 @@ object MBTA_Arrivals {
     val events: Future[immutable.Seq[ServerSentEvent]] =
     eventSource
       .throttle(elements = 1, per = 500.milliseconds, maximumBurst = 1, ThrottleMode.Shaping)
-      .take(50)
+      .take(3)
       .runWith(Sink.seq)
 
     val result = Await.ready(events, Duration.Inf).value.get
