2d1ac822265172107f2a792be35a24a2f3c3b006
James Nicholson <jnicholson@hsph.harvard.edu>
2022-12-06 11:55:41 -0500

    added comments

--- src/main/scala/arrivals/Arrivals.scala
+++ src/main/scala/arrivals/Arrivals.scala
@@ -19,6 +19,7 @@ import scala.concurrent.{Await, Future}
 import scala.concurrent.duration.Duration
 import com.typesafe.scalalogging.{LazyLogging}
 
+// App config case class.
 case class ArrivalsAppConfig(
   predictionsURI: String,
   APIKey: String
@@ -26,17 +27,21 @@ case class ArrivalsAppConfig(
 
 object MBTA_Arrivals {
 
+  // Make config implicit.
   implicit val conf = ConfigUtils.loadAppConfig[ArrivalsAppConfig]("arrivals")
 
   import AkkaStreamUtils.defaultActorSystem._
 
+  // Function required by EventSource to send http requests.
   def sendHttp(req: HttpRequest): Future[HttpResponse] = {
+    // Add headers specific to MBTA API.
     val req_with_headers = req.withHeaders(
       List(
         RawHeader("Accept", "text/event-stream"),
         RawHeader("X-API-Key", conf.APIKey)
       )
     )
+    // Send the request.
     val responseFuture: Future[HttpResponse] = Http().singleRequest(req_with_headers)
     responseFuture
       .onComplete {
@@ -48,6 +53,7 @@ object MBTA_Arrivals {
 
   def main(args: Array[String]): Unit = {
 
+    // Open the SSE connection and create a source from received events.
     val eventSource: Source[ServerSentEvent, NotUsed] =
       EventSource(
         uri = Uri(conf.predictionsURI),
@@ -56,12 +62,14 @@ object MBTA_Arrivals {
         retryDelay = 1.second
       )
 
+    // Materialize events into a sequence of three events.
     val events: Future[immutable.Seq[ServerSentEvent]] =
     eventSource
       .throttle(elements = 1, per = 500.milliseconds, maximumBurst = 1, ThrottleMode.Shaping)
       .take(3)
       .runWith(Sink.seq)
 
+    // Wait for events to be ready, then print them to console.
     val result = Await.ready(events, Duration.Inf).value.get
     result.foreach(println)
   }
