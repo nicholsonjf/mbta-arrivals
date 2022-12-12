d2618f11b5423f880612d8c1681825e506d5b795
James Nicholson <jnicholson@hsph.harvard.edu>
2022-12-11 20:49:48 -0800

    Pre submit

--- src/main/scala/arrivals/Arrivals.scala
+++ src/main/scala/arrivals/Arrivals.scala
@@ -47,7 +47,7 @@ object MBTA_Arrivals {
 
   import AkkaStreamUtils.defaultActorSystem._
 
-  // Function required by EventSource to send http requests.
+  // Creates an Akka source from the MBTA raw data file.
   def rawDataStream(path: String): Source[ByteString, Future[IOResult]] = {
 
     val file = Paths.get(path)
@@ -57,22 +57,26 @@ object MBTA_Arrivals {
     ioRes
   }
 
+  // An in-memory storage structure for Predictions already encountered by the stream.
   implicit var pmap = mutable.Map.empty[String, Prediction]
 
+  // Update a prediction already present in the map with the new prediction. Print whether the train is running early or late.
   def updatePrediction(new_prediction: Prediction, orig_prediction: Prediction)(implicit predictionMap: mutable.Map[String, Prediction]): Unit = {
     val vehicle_id = orig_prediction.relationships.vehicle.data.id
+    val stop_id = orig_prediction.relationships.stop.data.id
     val orig_arrival_time = LocalDateTime.parse(orig_prediction.attributes.arrival_time, DateTimeFormatter.ISO_DATE_TIME)
     val new_arrival_time = LocalDateTime.parse(new_prediction.attributes.arrival_time, DateTimeFormatter.ISO_DATE_TIME)
     val difference = orig_arrival_time.until(new_arrival_time, SECONDS);
     val abs_difference = abs(difference)
     difference match {
-      case i: Long if i > 0 => println(s"Train $vehicle_id is running $abs_difference seconds late")
-      case j: Long if j < 0 => println(s"Train $vehicle_id is running $abs_difference seconds early")
+      case i: Long if i > 0 => println(s"Train $vehicle_id is running $abs_difference seconds late to stop $stop_id (was arriving at $orig_arrival_time now $new_arrival_time)")
+      case j: Long if j < 0 => println(s"Train $vehicle_id is running $abs_difference seconds early to stop $stop_id (was arriving at $orig_arrival_time now $new_arrival_time)")
       case _ => ()
     }
     pmap.update(orig_prediction.id, new_prediction)
   }
 
+  // Used to create a native "Prediction" type from a string of JSON.
   case class Prediction(attributes: Attributes_1, id: String, relationships: Relationships_1)
   case class Attributes_1(arrival_time: String)
   case class Relationships_1(stop: Stop_2, vehicle: Vehicle_2)
@@ -81,6 +85,7 @@ object MBTA_Arrivals {
   case class Vehicle_2(data: VehicleData_3)
   case class VehicleData_3(id: String)
 
+  // Needed by Spray JSON to parse a string of API data into a native Prediction.
   object MyJsonProtocol extends DefaultJsonProtocol {
     implicit val vehicleData3Format = jsonFormat1(VehicleData_3)
     implicit val vehicle2Format = jsonFormat1(Vehicle_2)
@@ -91,6 +96,7 @@ object MBTA_Arrivals {
     implicit val predictionFormat = jsonFormat3(Prediction)
   }
 
+  // Used to ignore Predictions that don't have values for required fields.
   def validPrediction(prediction: Prediction): Option[Prediction] = {
     val hasId = prediction.id.size > 0
     val hasArrivalTime = prediction.attributes.arrival_time.size > 0
@@ -103,6 +109,8 @@ object MBTA_Arrivals {
     }
   }
 
+  // Handler function for a valid prediction. If it's new prediction, print the user friendly prediction.
+  // If it's an existing prediction, pass it to the updatePrediction function.
   def handlePrediction(prediction: Prediction): Unit = {
     val vehicle_id = prediction.relationships.vehicle.data.id
     val stop_id = prediction.relationships.stop.data.id
@@ -116,18 +124,27 @@ object MBTA_Arrivals {
     }
   }
 
+  // Main application entrypoint.
   def main(args: Array[String]): Unit = {
     import MyJsonProtocol._
 
+    // Create a source from a text file of cleaned up MBTA predictions.
     val eSource: Source[ByteString, Future[IOResult]] = 
       rawDataStream(conf.eventsFilePath)
 
+    // Create a flow.
+    // Parse the bytestring into into lines separated by newline characters.
+    // Return the utf8 String of each bytestring.
+    // Use back pressure to throttle the RunnableGraph to 1 item per second to simulate the speed of the MBTA streaming API.
     val eFlow = 
       Flow[ByteString]
         .via(Framing.delimiter(delimiter = ByteString(System.lineSeparator), maximumFrameLength = 10000, allowTruncation = true))
         .map(byteString => byteString.utf8String)
         .throttle(elements = 1, per = 1.second, maximumBurst = 1, mode = ThrottleMode.shaping)
 
+    // Create a sink.
+    // Parse each String from JSON to the native Prediction type.
+    // Only handle valid Predictions.
     val eSink = Sink.foreach({
       (bs: String) =>
         val ast = bs.parseJson
@@ -139,8 +156,10 @@ object MBTA_Arrivals {
         }
     })
 
+    // Run the flow
     val f = eSource.via(eFlow).runWith(eSink)
 
+    // Block on the completion of running the flow.
     Await.result(f, Duration.Inf)
 
   }
